95e10b6d01ac50d9957508bb34136c9c38bd25d4
zhacongjie <zhacongjie@qgutech.com>
2017-12-22 16:54:04 +0800

    授权回调第一步

--- src/main/kotlin/com/kotlin/zcj/tc/tiancai/controller/IndexController.kt
+++ src/main/kotlin/com/kotlin/zcj/tc/tiancai/controller/IndexController.kt
@@ -9,6 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.ModelAndView
 import javax.annotation.Resource
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import com.alipay.api.AlipayApiException
+import com.alipay.api.response.AlipaySystemOauthTokenResponse
+import com.alipay.api.request.AlipaySystemOauthTokenRequest
+import com.alipay.api.AlipayConstants.APP_ID
+import com.alipay.api.DefaultAlipayClient
+import com.alipay.api.AlipayClient
+import com.kotlin.zcj.tc.tiancai.alipay.config.AlipayConfig
+import com.kotlin.zcj.tc.tiancai.utils.TcConstants
+
 
 /**
  * @Since2017/12/9 ZhaCongJie@HF
@@ -23,10 +34,29 @@ class IndexController {
 
     @RequestMapping("/index.html")
     @ResponseBody
-    fun index(): ModelAndView {
+    fun index(request: HttpServletRequest, response: HttpServletResponse): ModelAndView {
 //        val user: TTcUserRecord = userService.load("86e61c732d4242aaadebab031a600191");
 //        stringRedis.opsForValue().set(user.userId, user.toString());
 //        println(stringRedis.opsForValue().get(user.userId));
+        val authCode = request.getParameter("auth_code");
+        val scope = request.getParameter("scope");
+        val alipayClient = DefaultAlipayClient("https://openapi.alipay.com/gateway.do",AlipayConfig.appId, AlipayConfig.aesKey
+        ,"json",AlipayConfig.input_charset,AlipayConfig.publicKey,"RSA2")
+        val aliRequest = AlipaySystemOauthTokenRequest()
+        aliRequest.code = authCode
+        aliRequest.grantType = "authorization_code"
+        try {
+            val aliResponse = alipayClient.execute(aliRequest)
+            if(aliResponse.isSuccess){
+                System.out.println("调用成功");
+            } else {
+                System.out.println("调用失败");
+            }
+        } catch (e: AlipayApiException) {
+            //处理异常
+            e.printStackTrace()
+        }
+
         return ModelAndView("index");
     }
 
